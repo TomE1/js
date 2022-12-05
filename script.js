4abc300154cb9eb06f21bc61625e97fd66a5db3b
TomE <tommevans210@gmail.com>
2022-12-05 13:47:48 +0700

    random number generator

--- script.js
+++ script.js
@@ -29,12 +29,18 @@ function setColor() {
   }
 }
 
-// add.addEventListener('click', () => {
-//   count.innerHTML++;
-// });
-// sub.addEventListener('click', () => {
-//   count.innerHTML--;
-// });
-// resetCount.addEventListener('click', () => {
-//   count.innerHTML = 0;
-// });
+// random number generator
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
+
+const number = document.querySelector('.number');
+const btn = document.querySelector('.generate');
+
+const generateNumber = () => {
+  // Generate number btn 1 and 10
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.innerHTML = randomNumber;
+};
+
+btn.addEventListener('click', generateNumber);
